8e171e9dca87b477ea8d58c8529880db34adbee3
Conner <hetti031@morris.umn.edu>
2022-01-26 15:55:41 -0600

    Committing the Unity project

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    Transform mainTransform;
+    public float moveSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainTransform = GetComponent<Transform>();
+        StartCoroutine(FireRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator FireRoutine() {
+        for(int i = 2000; i >0; i--){
+            mainTransform.position += mainTransform.up * moveSpeed * Time.deltaTime;
+            yield return new WaitForSeconds(.01f);
+        }
+        Destroy(this.gameObject);
+        yield break;
+    }
+}
---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+
+{
+    [SerializeField] private Transform gunTransform;
+    [SerializeField] private Transform shootPosition;
+    [Range(0, 200)]
+    [SerializeField] private float rotationSpeed;
+
+    [SerializeField] private GameObject[] bullets;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gunTransform.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            fire();
+        }
+    }
+
+    public void fire(){
+        GameObject bulletPrefab = bullets[Random.Range(0, bullets.Length)];
+        GameObject newBullet = Instantiate(bulletPrefab);
+        newBullet.transform.SetPositionAndRotation(shootPosition.position, shootPosition.rotation);
+
+    }
+    
+}
