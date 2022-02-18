2e9594f29a70deb7da134723533d2dedd2b7b8be
Conner <hetti031@morris.umn.edu>
2022-02-18 01:14:18 -0600

    Paddles have parameter that tells them when to stop on the y axis

--- Assets/Script/PaddleControl.cs
+++ Assets/Script/PaddleControl.cs
@@ -10,6 +10,7 @@ public class PaddleControl : MonoBehaviour
     [SerializeField] private Transform paddleTransform;
     [SerializeField] private int moveSpeed;
     [SerializeField] private float ceiling;
+    [SerializeField] private float floor;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +20,21 @@ public class PaddleControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float currentY = paddleTransform.position.y;
         if (Input.GetKey(upKey))
         {
-            float currentY = paddleTransform.position.y;
             float nextPosition = Mathf.Min(ceiling, currentY);
-            // figure out how to create a new transform or something
-            paddleTransform = new Vector3(paddleTransform.position.x, nextPosition, paddleTransform.position.z);
+            if(nextPosition == currentY){
             paddleTransform.position = paddleTransform.position + new Vector3(0, moveSpeed * Time.deltaTime, 0);
+            }
         }
 
         if (Input.GetKey(downKey))
         {
+            float nextPosition = Mathf.Max(floor, currentY);
+            if(nextPosition == currentY){
             paddleTransform.position = paddleTransform.position + new Vector3(0, -moveSpeed * Time.deltaTime, 0);
+            }
         }
 
     }
