2af7f6fedcd1f138a1532d6eb65b7da62ca48442
Conner <hetti031@morris.umn.edu>
2022-02-21 22:17:58 -0600

    Add rest of pong functionality

--- Assets/Script/PongBall.cs
+++ Assets/Script/PongBall.cs
@@ -6,7 +6,7 @@ public class PongBall : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody2D mainRigidbody;
-    [SerializeField] private BoxCollider2D mainBoxCollider;
+    [SerializeField] private PongManager mainPongManager;
     [SerializeField] private int startSpeed;
     private WaitForSeconds restartWait = new WaitForSeconds(1f);
 
@@ -19,7 +19,6 @@ public class PongBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
 
@@ -32,8 +31,11 @@ public class PongBall : MonoBehaviour
 
     IEnumerator startBall()
     {
+        yield return restartWait;
+        if(mainPongManager.redScore < 10 && mainPongManager.blueScore < 10) 
+        {
         Vector2 newVelocity = new Vector2(Random.Range(-5f, 5f), Random.Range(-1f, 1f));
         mainRigidbody.velocity = newVelocity.normalized * startSpeed;
-        yield return restartWait;
+        }
     }
 }
--- Assets/Script/PongManager.cs
+++ Assets/Script/PongManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PongManager : MonoBehaviour
 
@@ -9,33 +10,58 @@ public class PongManager : MonoBehaviour
 
     [SerializeField] private PongBall ball;
     [SerializeField] private PongGoal p1Goal;
-    [SerializeField] private TextMeshProUGUI textDisplay;
+    [SerializeField] private PongGoal p2Goal;
+    [SerializeField] private TextMeshProUGUI playerOneScore;
+    [SerializeField] private TextMeshProUGUI playerTwoScore;
+    [SerializeField] private TextMeshProUGUI victoryText;
     public int redScore = 0;
+    public int realRedScore;
     public int blueScore = 0;
-    // Start is called before the first frame update
+    public int realBlueScore;
+
+    private WaitForSeconds restartWait = new WaitForSeconds(5f);
     void Start()
     {
         Awake();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        textDisplay.text = redScore.ToString();
+        realRedScore = redScore/2;
+        realBlueScore = blueScore/2;
+        playerOneScore.text = "Player 1: " + realRedScore.ToString();
+        playerTwoScore.text = "Player 2: " + realBlueScore.ToString();
+
+        if(redScore == 10) {
+        victoryText.text = "Player 1 Wins!";
+        StartCoroutine(restartGame());
+        }
+
+        if(blueScore == 10) {
+        victoryText.text = "Player 2 Wins!";
+        StartCoroutine(restartGame());
+        }
     }
 
     void HandleP1Score() {
         redScore++;
+        ball.Restart();
     }
 
     void HandleP2Score() {
-        
+        blueScore++;
+        ball.Restart();
     }
 
     private void Awake() {
-    p1Goal.onScore += HandleP2Score;
-    /*p2Goal.onScore += HandleP1Score;*/
+    p2Goal.onScore += HandleP2Score;
+    p1Goal.onScore += HandleP1Score;
     ball.Restart();
- 
+    }
+      IEnumerator restartGame() {
+        yield return restartWait;
+        redScore = 0;
+        blueScore = 0;
+        victoryText.text = "";
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
