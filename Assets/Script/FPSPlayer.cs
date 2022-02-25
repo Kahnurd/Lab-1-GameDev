9b4a52fd4da11b76932a0518a823f0feae142b27
Colt Dahl <dahl1173@morris.umn.edu>
2022-02-25 12:19:48 -0600

    Add ShouldSpawn, health init, collision

--- Assets/Script/FPSPlayer.cs
+++ Assets/Script/FPSPlayer.cs
@@ -9,13 +9,20 @@ public class FPSPlayer : MonoBehaviour
     [SerializeField] private Transform head;
     [SerializeField] private GameObject[] bullets;
     [SerializeField] private AudioSource shootSound;
-    //[SerializeField] private FPSUI fpsUI;
+    [SerializeField] private FPSUI fpsUI;
     [SerializeField] private int maxHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public static FPSPlayer instance;
+
+    private void Awake()
+    {
+        instance = this;
     }
 
     // Update is called once per frame
@@ -28,7 +35,54 @@ public class FPSPlayer : MonoBehaviour
             newBullet.transform.SetPositionAndRotation(shootPosition.position, shootPosition.rotation);
             shootSound.Play();
         }
+    }
 
+    private float lastHitTime;
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Enemy") && (Time.time - lastHitTime > 1f))
+        {
+            lastHitTime = Time.time;
+            Destroy(hit.gameObject);
+            if (Health > 0)
+            {
+                Health--;
+            }
+        }
+    }
+
+    private int health;
+    private int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            fpsUI.ShowHealthFraction((float)Health / (float)maxHealth);
+            if(health <= 0)
+            {
+                LoadingScreen.LoadScene("MainMenu");
+            }
+        }
+    }
+
+    private int enemyDefeatCount;
+    public void HandleEnemyDefeat()
+    {
+        enemyDefeatCount++;
+        fpsUI.ShowEnemyDefeatCount(enemyDefeatCount);
+    }
+
+    public bool ShouldSpawn(Vector3 pos)
+    {
+        Vector3 posDiff = pos - transform.position;
+        Vector3 faceDirection = head.forward;
+        float distanceFromPlayer = posDiff.magnitude;
+        return (Vector3.Dot(posDiff.normalized, faceDirection) < 0.5f) && (distanceFromPlayer > 10f);
 
     }
+
 }
