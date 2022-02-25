faec2c6c60b78dc4ab76423f6f02bd524f2835a7
Colt Dahl <dahl1173@morris.umn.edu>
2022-02-25 12:03:14 -0600

    Add gun, add script for shooting bullets

---
+++ Assets/Script/FPSPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSPlayer : MonoBehaviour
+{
+
+    [SerializeField] private Transform shootPosition;
+    [SerializeField] private Transform head;
+    [SerializeField] private GameObject[] bullets;
+    [SerializeField] private AudioSource shootSound;
+    //[SerializeField] private FPSUI fpsUI;
+    [SerializeField] private int maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletPrefab = bullets[Random.Range(0, bullets.Length)];
+            GameObject newBullet = Instantiate(bulletPrefab);
+            newBullet.transform.SetPositionAndRotation(shootPosition.position, shootPosition.rotation);
+            shootSound.Play();
+        }
+
+
+    }
+}
