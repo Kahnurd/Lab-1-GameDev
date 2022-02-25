9647c9d5f065db25aa975d9c6ac48b10baa41470
Conner <hetti031@morris.umn.edu>
2022-02-25 12:17:41 -0600

    add FPSUI
    Health bar and Enemy count methods under a canvas with an image and a TextMeshPro

---
+++ Assets/Script/FPSUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FPSUI : MonoBehaviour
+{
+
+    [SerializeField] private Image healthBar;
+    [SerializeField] private TMP_Text enemyDefeatText;
+    public void ShowHealthFraction(float fraction)
+    {
+        healthBar.fillAmount = fraction;
+    }
+
+    public void ShowEnemyDefeatCount(int count)
+    {
+        enemyDefeatText.text = "Enemies Defeated: " + count;
+    }
+}
